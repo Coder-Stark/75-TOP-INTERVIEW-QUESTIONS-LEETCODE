0a0e2d22258756fc9eba443b155562b8218ee64d
Coder-Stark <shivamkumar789321@gmail.com>
2024-01-15 15:02:48 +0530

    Queue Completed

--- 75 LeetCode Questions.cpp
+++ 75 LeetCode Questions.cpp
@@ -1109,3 +1109,205 @@ Input: asteroids = [10,2,-5]
 Output: [10]
 Explanation: The 2 and -5 collide resulting in -5. The 10 and -5 collide resulting in 10.
 */
+
+
+//26. DECODE STRINGS                                                 {T.C = O(N), S.C = O(N)}
+/*
+Use two stacks(string, int), 2 var(string, int) iterate string , handle cases opening bracket= push curr string and 
+curr number in respective stacks, closing brackets = pops the previous string and number from their respective stacks
+isdigit() = currNum = currNum * 10 + (it - '0'), else add rest of char and return currStr.
+*/
+class Solution {
+public:
+    string decodeString(string s) {
+        string currStr="";
+        int currNum = 0;
+        stack<string>strStk;
+        stack<int>numStk;
+
+        for(auto it : s){
+            if(it == '['){
+                strStk.push(currStr);
+                numStk.push(currNum);
+                currStr = "";              //reset after pushing
+                currNum = 0;               //reset after pushing
+            }else if(it == ']'){           //pops the previous string and number from their respective stacks
+                int num = numStk.top();    
+                numStk.pop();
+                string outPut = "";
+                while(num--){
+                    outPut += currStr;
+                }
+
+                string prevStr = strStk.top();
+                strStk.pop();
+                currStr = prevStr + outPut;
+            }else if(isdigit(it)){
+                currNum = currNum * 10 + (it - '0');
+            }else{
+                currStr += it;
+            }
+        }
+        return currStr;
+    }
+};
+/*
+Example 1:
+Input: s = "3[a]2[bc]"
+Output: "aaabcbc"
+
+Example 2:
+Input: s = "3[a2[c]]"
+Output: "accaccacc"
+
+Example 3:
+Input: s = "2[abc]3[cd]ef"
+Output: "abcabccdcdcdef"
+*/
+
+
+/*--------------------------------------------------- QUEUE ----------------------------------------------------*/
+//27. NUMBER OF RECENT CALLS                                             {T.C = O(N), S.C = O(1)}
+/*
+Take an queue, first find the margin(t-3000) push the element(t) in queue, pop the element from queue until top element lesser then margin
+after that return size of queue.
+*/
+class RecentCounter {
+public:
+    queue<int>q;
+    RecentCounter() {
+        
+    }
+    int ping(int t) {
+        int margin = t - 3000;
+        q.push(t);
+        while(q.front() < margin){
+            q.pop();
+        }
+        return q.size();
+    }
+};
+/*
+Example 1:
+
+Input
+["RecentCounter", "ping", "ping", "ping", "ping"]
+[[], [1], [100], [3001], [3002]]
+Output
+[null, 1, 2, 3, 3]
+
+Explanation
+RecentCounter recentCounter = new RecentCounter();
+recentCounter.ping(1);     // requests = [1], range is [-2999,1], return 1
+recentCounter.ping(100);   // requests = [1, 100], range is [-2900,100], return 2
+recentCounter.ping(3001);  // requests = [1, 100, 3001], range is [1,3001], return 3
+recentCounter.ping(3002);  // requests = [1, 100, 3001, 3002], range is [2,3002], return 3
+*/
+
+
+//28. DOTA2 SENAT
+//BRUTE FORCE                                                         {T.C = O(N^2), S.C = O(1)}
+/*
+First store count of R and D, initialize idx = 0, if (R) remove nearest D vice versa,
+idx-- when we deleted someone at index < idx.
+idx+1 % (length of updated strings) for round.
+*/
+class Solution {
+public:
+    bool removeSenator(string &senate, char ch, int i){
+        bool checkRemoveLeftSide = false;
+
+        while(true){
+            if(i == 0){              //1 round completed
+                checkRemoveLeftSide = true;
+            }
+            if(senate[i] == ch){
+                senate.erase(senate.begin() + i);  //shift
+                break;
+            }
+            (i+1) % senate.length();
+        }
+        return checkRemoveLeftSide;
+    }
+    string predictPartyVictory(string senate) {
+        int rCount = count(senate.begin(), senate.end(), 'R');
+        int dCount = senate.length()-rCount;
+
+        int idx = 0;
+        while(rCount > 0 && dCount > 0){
+            if(senate[idx] == 'R'){
+                bool checkRemoveLeftSide = removeSenator(senate, 'D',(idx+1) % senate.length());
+                dCount--;
+                if(checkRemoveLeftSide){
+                    idx--;
+                }
+            }else{ //(senate[idx] == 'D')
+                bool checkRemoveLeftSide = removeSenator(senate, 'R', (idx+1) % senate.length());
+                rCount--;
+                if(checkRemoveLeftSide){
+                    idx--;
+                }
+            }
+            idx = (idx+1) % senate.length();
+        }
+
+        return rCount == 0 ? "Dire" : "Radiant";
+    }
+};
+
+//OPTIMIZED APPROACH                                                       {T.C = O(N), S.C = O(N)}
+/*
+Similar to above just not shifting element,by taking extra space of vector <bool>removed.
+*/
+class Solution {
+public:
+    void removeSenator(string &senate, char ch, int i, vector<bool>&removed){
+        while(true){
+            if(senate[i] == ch && removed[i] == false){
+                removed[i] = true;
+                break;
+            }
+            i = (i+1) % senate.length();
+        }
+    }
+    string predictPartyVictory(string senate) {
+        int rCount = count(senate.begin(), senate.end(), 'R');
+        int dCount = senate.length()-rCount;
+        int n = senate.length();
+        vector<bool>removed(n, false);
+
+        int idx = 0;
+        while(rCount > 0 && dCount > 0){
+            if(removed[idx] == false){
+                if(senate[idx] == 'R'){
+                    removeSenator(senate, 'D',(idx+1) % senate.length(), removed);
+                    dCount--;
+                }else{ //(senate[idx] == 'D')
+                    removeSenator(senate, 'R', (idx+1) % senate.length(), removed);
+                    rCount--;
+                }
+            }
+            idx = (idx+1) % senate.length();
+        }
+
+        return rCount == 0 ? "Dire" : "Radiant";
+    }
+};
+/*
+Example 1:
+Input: senate = "RD"
+Output: "Radiant"
+Explanation: 
+The first senator comes from Radiant and he can just ban the next senator's right in round 1. 
+And the second senator can't exercise any rights anymore since his right has been banned. 
+And in round 2, the first senator can just announce the victory since he is the only guy in the senate who can vote.
+
+Example 2:
+Input: senate = "RDD"
+Output: "Dire"
+Explanation: 
+The first senator comes from Radiant and he can just ban the next senator's right in round 1. 
+And the second senator can't exercise any rights anymore since his right has been banned. 
+And the third senator comes from Dire and he can ban the first senator's right in round 1. 
+And in round 2, the third senator can just announce the victory since he is the only guy in the senate who can vote.
+*/
